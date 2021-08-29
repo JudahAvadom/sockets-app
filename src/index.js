a6366c48576326afb70f0ef0a66e6441cf04a861
RedCode26 <judaslinarez@gmail.com>
2021-08-29 16:20:15 -0500

    Chat

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 const express=require("express");
 const path=require("path")
 const app=express();
+const SocketIO=require("socket.io")
 
 app.set("port", process.env.PORT || 3000)
 
-app.use(express.static())
+app.use(express.static(path.join(__dirname, "public")))
 
-app.listen(app.get("port"),()=>{
+const server=app.listen(app.get("port"),()=>{
     console.log(`Server on port ${app.get("port")}`);
+})
+const io=SocketIO(server);
+io.on("connection",(socket)=>{
+    console.log("New Connection",socket.id);
+    socket.on('chat:message',(data)=>{
+        io.sockets.emit('chat:message',data)
+    })
+    socket.on("chat:typing",(data)=>{
+        socket.broadcast.emit('chat:typing',data)
+    })
 })
---
+++ src/public/chat.js
@@ -0,0 +1,26 @@
+const socket=io()
+
+let message=document.getElementById("message")
+let username=document.getElementById("username")
+let btn=document.getElementById("send");
+let output=document.getElementById("output")
+let actions=document.getElementById("actions")
+
+btn.addEventListener('click',()=>{
+    socket.emit('chat:message',{
+        message:message.value,
+        username:username.value
+    })
+})
+message.addEventListener('keypress',()=>{
+    socket.emit("chat:typing",username.value)
+})
+socket.on('chat:message',(data)=>{
+    actions.innerHTML="";
+    output.innerHTML+=/*html*/`<p>
+        <strong>${data.username}</strong>: ${data.message}
+    </p>`
+})
+socket.on("chat:typing",(data)=>{
+    actions.innerHTML=/*html*/`<p><em>${data} Está escribiendo...</em></p>`;
+})
